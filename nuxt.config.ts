f9764be00ba815d399e08bef7a8f358b83f046ad
Lukas Trumm <lukas.trumm@gmail.com>
2024-05-17 15:14:40 +0200

    feat: add Nuxt Hub module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,12 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/ui", "nuxt-headlessui"],
+  modules: ["@nuxt/eslint", "@nuxt/ui", "nuxt-headlessui", "@nuxthub/core"],
   headlessui: {
     prefix: "H",
+  },
+  hub: {
+    kv: true,
+    cache: true,
   },
   vite: {
     vue: {
