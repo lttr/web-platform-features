28c6e4b3248ff2a89fd9b4703731078f3abb29ec
Lukas Trumm <lukas.trumm@gmail.com>
2024-07-08 14:36:01 +0200

    Add Nuxt compat date config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,16 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/eslint", "@nuxt/ui", "nuxt-headlessui", "@nuxthub/core"],
+
   headlessui: {
     prefix: "H",
   },
+
   hub: {
     kv: true,
     cache: true,
   },
+
   vite: {
     vue: {
       script: {
@@ -15,10 +18,13 @@ export default defineNuxtConfig({
       },
     },
   },
+
   $production: {
     routeRules: {
       // Features page generated on demand, revalidates in background, cached for 1 hour
       "/": { swr: 60 * 60 },
     },
   },
+
+  compatibilityDate: "2024-07-08",
 })
