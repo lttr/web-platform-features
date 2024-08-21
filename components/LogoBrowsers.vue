6a00a4813353c650f1837fc4cc8633b23d13c54c
Lukas Trumm <lukas.trumm@gmail.com>
2024-08-21 13:09:10 +0200

    Add image dimensions

--- components/LogoBrowsers.vue
+++ components/LogoBrowsers.vue
@@ -1,20 +1,68 @@
 <template>
   <div class="flex flex-row flex-wrap gap-1 md:gap-3 p-1">
     <div class="flex h-5">
-      <img class="h-5" src="~/assets/baseline/chrome.svg" alt="" />
-      <img class="h-5" src="~/assets/baseline/browser-check.svg" alt="" />
+      <img
+        width="20"
+        height="20"
+        class="h-5"
+        src="~/assets/baseline/chrome.svg"
+        alt=""
+      />
+      <img
+        width="20"
+        height="20"
+        class="h-5"
+        src="~/assets/baseline/browser-check.svg"
+        alt=""
+      />
     </div>
     <div class="flex h-5">
-      <img class="h-5" src="~/assets/baseline/edge.svg" alt="" />
-      <img class="h-5" src="~/assets/baseline/browser-check.svg" alt="" />
+      <img
+        width="20"
+        height="20"
+        class="h-5"
+        src="~/assets/baseline/edge.svg"
+        alt=""
+      />
+      <img
+        width="20"
+        height="20"
+        class="h-5"
+        src="~/assets/baseline/browser-check.svg"
+        alt=""
+      />
     </div>
     <div class="flex h-5">
-      <img class="h-5" src="~/assets/baseline/firefox.svg" alt="" />
-      <img class="h-5" src="~/assets/baseline/browser-check.svg" alt="" />
+      <img
+        width="20"
+        height="20"
+        class="h-5"
+        src="~/assets/baseline/firefox.svg"
+        alt=""
+      />
+      <img
+        width="20"
+        height="20"
+        class="h-5"
+        src="~/assets/baseline/browser-check.svg"
+        alt=""
+      />
     </div>
     <div class="flex h-5">
-      <img class="h-5" src="~/assets/baseline/safari.svg" alt="" />
-      <img class="h-5" src="~/assets/baseline/browser-check.svg" alt="" />
+      <img
+        width="20"
+        height="20"
+        class="h-5"
+        src="~/assets/baseline/safari.svg"
+        alt=""
+      />
+      <img
+        width="20"
+        height="20"
+        class="h-5"
+        src="~/assets/baseline/browser-check.svg"
+        alt=""
+      />
     </div>
   </div>
 </template>
